9f989ee9764f9bec3ce1a368dbbaa47e4b264bc4
kziad551 <96523129+kziad551@users.noreply.github.com>
2023-08-24 10:20:34 +0300

    Update index.js

--- pages/visits/index.js
+++ pages/visits/index.js
@@ -184,7 +184,7 @@ export default Visits;
 
 export async function getServerSideProps() {
   try {
-    const apiUrl = "https://heritage1.top-wp.com/api/visitss";
+    const apiUrl = "https://heritage1.top-wp.com/api/visits";
     const response = await fetch(apiUrl);
 
     if (!response.ok) {
